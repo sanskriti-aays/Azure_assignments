0adcecfdca81dc0222e8f216e37f591b85da8046
sanskriti-aays <sanskritig@aaysinsight.com>
2024-08-01 18:47:16 +0530

    Update sql_assignment(1-aug).sql

--- sql_assignment(1-aug).sql
+++ sql_assignment(1-aug).sql
@@ -20,6 +20,44 @@ SELECT
 FROM salaries
 GROUP BY SalaryRange;
 
+CREATE VIEW view_employees AS
+SELECT emp_no, first_name, last_name, gender, hire_date
+FROM employees
+WHERE emp_no IN (10001, 10002, 10003, 10004, 10005);
+
+CREATE VIEW view_dept_emp AS
+SELECT emp_no, dept_no, from_date, to_date
+FROM dept_emp
+WHERE emp_no IN (10004, 10005, 10006, 10007, 10008);
+
+SELECT 
+    e.emp_no AS employee_emp_no,
+    e.first_name,
+    e.last_name,
+    e.gender,
+    e.hire_date,
+    d.emp_no AS dept_emp_emp_no,
+    d.dept_no,
+    d.from_date,
+    d.to_date
+FROM view_employees e
+LEFT JOIN view_dept_emp d ON e.emp_no = d.emp_no
+ 
+UNION
+ 
+SELECT 
+    e.emp_no AS employee_emp_no,
+    e.first_name,
+    e.last_name,
+    e.gender,
+    e.hire_date,
+    d.emp_no AS dept_emp_emp_no,
+    d.dept_no,
+    d.from_date,
+    d.to_date
+FROM view_employees e
+RIGHT JOIN view_dept_emp d ON e.emp_no = d.emp_no;
+
 
  
 
